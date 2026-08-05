a27ec5c062185c992cf412414e24b7569f056480
tuconta360ec <sakemusicjesus@gmail.com>
2026-08-05 10:48:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,33 @@ enlaces.forEach(enlace => {
     });
 
 });
+function calcularImpuesto(){
+
+    let ingresos = Number(document.getElementById("ingresos").value);
+
+    let regimen = document.getElementById("regimen").value;
+
+    let impuesto = 0;
+
+    if(regimen=="popular"){
+
+        impuesto = 0;
+
+    }
+
+    else if(regimen=="emprendedor"){
+
+        impuesto = ingresos*0.02;
+
+    }
+
+    else{
+
+        impuesto = ingresos*0.25;
+
+    }
+
+    document.getElementById("resultado").innerHTML =
+    "Impuesto estimado: $ " + impuesto.toFixed(2);
+
+}
